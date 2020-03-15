f2df4260905c2e32d674f366b5c12e139170b501
Luukuton <mleinikka@gmail.com>
2020-03-15 21:43:56 +0200

    Lisätty toiminnallisuus kategorioiden lisäämiselle, listaamiselle ja muokkaamiselle sekä päivitetty vaatimukset (requirements.txt)

---
+++ application/categories/views.py
@@ -0,0 +1,33 @@
+from application import app, db
+from flask import redirect, render_template, request, url_for
+from application.categories.models import Category
+
+
+@app.route("/categories", methods=["GET"])
+def categories_index():
+    return render_template("categories/list.html", categories=Category.query.all())
+
+
+@app.route("/categories/new/")
+def categories_form():
+    return render_template("categories/new.html")
+
+
+@app.route("/categories/<category_id>/", methods=["POST"])
+def categories_edit(category_id):
+    c = Category.query.get(category_id)
+    c.title = request.form.get("name")
+    c.description = request.form.get("description")
+    db.session().commit()
+
+    return redirect(url_for("categories_index"))
+
+
+@app.route("/categories/", methods=["POST"])
+def categories_create():
+    c = Category(request.form.get("name"), request.form.get("description"))
+
+    db.session().add(c)
+    db.session().commit()
+
+    return redirect(url_for("categories_index"))
