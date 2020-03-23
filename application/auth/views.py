08441a0a4b67995f4c3fa21f0b70716ea6ccd4ee
Luukuton <mleinikka@gmail.com>
2020-03-23 22:08:32 +0200

    Lisätty toiminallisuus käyttäjäntunnusten luomiselle

--- application/auth/views.py
+++ application/auth/views.py
@@ -1,9 +1,9 @@
-from flask import render_template, request, redirect, url_for
-from flask_login import login_user, logout_user
+from flask import redirect, render_template, request, url_for
+from flask_login import login_user, logout_user, login_required
 
-from application import app
+from application import app, db
 from application.auth.models import User
-from application.auth.forms import LoginForm
+from application.auth.forms import LoginForm, RegisterForm
 
 
 @app.route("/auth/login", methods=["GET", "POST"])
@@ -18,11 +18,29 @@ def auth_login():
         return render_template("auth/loginform.html", form=form, error="No such username or password")
 
     login_user(user)
-    print("User " + user.nickname + " is authenticated")
     return redirect(url_for("index"))
 
 
 @app.route("/auth/logout")
+@login_required
 def auth_logout():
     logout_user()
     return redirect(url_for("index"))
+
+
+@app.route("/auth/register", methods=["GET", "POST"])
+def auth_register():
+    if request.method == "GET":
+        return render_template("auth/registerform.html", form=RegisterForm())
+
+    form = RegisterForm(request.form)
+
+    if not form.validate():
+        return render_template("auth/registerform.html", form=form)
+
+    u = User(form.nickname.data, form.username.data, form.password.data, form.language.data)
+
+    db.session().add(u)
+    db.session().commit()
+
+    return redirect(url_for("auth_login"))
