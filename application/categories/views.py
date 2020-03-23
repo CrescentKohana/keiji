56ba14e3182560d6a03c351d0c8565c15d281d59
Luukuton <mleinikka@gmail.com>
2020-03-23 15:42:40 +0200

    Lisätty datan validointi lisäysvaiheessa

--- application/categories/views.py
+++ application/categories/views.py
@@ -1,6 +1,7 @@
-from application import app, db
 from flask import redirect, render_template, request, url_for
+from application import app, db
 from application.categories.models import Category
+from application.categories.forms import CategoryForm
 
 
 @app.route("/categories", methods=["GET"])
@@ -10,14 +11,16 @@ def categories_index():
 
 @app.route("/categories/new/")
 def categories_form():
-    return render_template("categories/new.html")
+    return render_template("categories/new.html", form=CategoryForm())
 
 
 @app.route("/categories/<category_id>/", methods=["POST"])
 def categories_edit(category_id):
+    form = CategoryForm(request.form)
     c = Category.query.get(category_id)
-    c.name = request.form.get("name")
-    c.description = request.form.get("description")
+
+    c.name = form.name.data
+    c.description = form.description.data
     db.session().commit()
 
     return redirect(url_for("categories_index"))
@@ -25,7 +28,12 @@ def categories_edit(category_id):
 
 @app.route("/categories/", methods=["POST"])
 def categories_create():
-    c = Category(request.form.get("name"), request.form.get("description"))
+    form = CategoryForm(request.form)
+
+    if not form.validate():
+        return render_template("categories/new.html", form=form)
+
+    c = Category(form.name.data, form.description.data)
 
     db.session().add(c)
     db.session().commit()
