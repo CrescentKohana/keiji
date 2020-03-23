40226b32568d45dca8c11d46bf5c9939d128c143
Luukuton <mleinikka@gmail.com>
2020-03-23 22:47:01 +0200

    PostgreSQL-tuki lisätty

--- application/__init__.py
+++ application/__init__.py
@@ -1,12 +1,17 @@
 from flask import Flask
 app = Flask(__name__)
 
-# Database
 from flask_sqlalchemy import SQLAlchemy
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///categories.db"
-app.config["SQLALCHEMY_ECHO"] = True
-# SQLALCHEMY_TRACK_MODIFICATIONS would add significant overhead, so it is disabled.
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+import os
+if os.environ.get("HEROKU"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///categories.db"
+    app.config["SQLALCHEMY_ECHO"] = True
+    # SQLALCHEMY_TRACK_MODIFICATIONS would add significant overhead, so it is disabled.
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 
 db = SQLAlchemy(app)
 
@@ -37,4 +42,7 @@ def load_user(user_id):
     return User.query.get(user_id)
 
 
-db.create_all()
+try:
+    db.create_all()
+except():
+    pass
