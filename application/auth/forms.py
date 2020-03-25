d9cfe2503c4cb60d8acbddf56023360b7605ff40
Luukuton <mleinikka@gmail.com>
2020-03-25 20:16:52 +0200

    Lisätty lisää validointiehtoja ja lisäksi itse validointi on myös nyt muokkausvaiheessa

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -1,20 +1,24 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, validators
+from wtforms import PasswordField, StringField, SelectField, validators
 
 
 class LoginForm(FlaskForm):
-    username = StringField("Username")
-    password = PasswordField("Password")
+    username = StringField("Username", [validators.data_required()])
+    password = PasswordField("Password", [validators.data_required()])
 
     class Meta:
         csrf = False
 
 
 class RegisterForm(FlaskForm):
-    nickname = StringField("Nickname", [validators.Length(min=3)])
-    username = StringField("Username", [validators.Length(min=3)])
-    password = PasswordField("Password", [validators.Length(min=8)])
-    language = StringField("Language", [validators.Length(min=2)])
+    nickname = StringField("Nickname", [validators.Length(min=3), validators.data_required()])
+    username = StringField("Username", [validators.Length(min=3), validators.data_required()])
+    password = PasswordField("Password", [validators.Length(min=8), validators.data_required()])
+    language = SelectField(
+        "Language",
+        [validators.Length(min=2), validators.data_required()],
+        choices=[('en', 'English'), ('fi', 'Finnish'), ('jp', 'Japanese')]
+    )
 
     class Meta:
         csrf = False
--- application/categories/forms.py
+++ application/categories/forms.py
@@ -5,8 +5,14 @@ from wtforms import StringField, validators
 class CategoryForm(FlaskForm):
     # Minimum in name is only one because some languages such as Japanese can easily have
     # one character long words such as 絵 as in picture.
-    name = StringField("Category name", [validators.Length(min=1)])
-    description = StringField("Description", [validators.Length(min=3)])
+    name = StringField(
+        "Category name",
+        [validators.Length(min=1), validators.data_required()]
+    )
+
+    description = StringField(
+        "Description"
+    )
 
     class Meta:
         csrf = False
