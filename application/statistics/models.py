76fada98102d42ff26c7a1db0664a856b94f8999
Luukuton <mleinikka@gmail.com>
2020-04-02 22:53:25 +0300

    Lisätty statistiikkoihin yhteensä käytetty aika

--- application/statistics/models.py
+++ application/statistics/models.py
@@ -5,19 +5,6 @@ from sqlalchemy import text
 
 class Statistics:
     @staticmethod
-    def overall_time_spent(current_user_id):
-        query = text("SELECT SUM(E.duration) "
-                     "FROM Event as E "
-                     "LEFT JOIN Category as C ON C.id = E.category_id "
-                     "WHERE (C.account_id = :user_id) "
-                     "GROUP BY C.account_id;",
-                     current_user_id).params(user_id=current_user_id)
-
-        result = db.engine.execute(query)
-
-        return result
-
-    @staticmethod
     def time_spent_on_category(current_user_id):
         query = text("SELECT COALESCE(SUM(E.duration),0) "
                      "FROM Category as C "
@@ -31,6 +18,6 @@ class Statistics:
 
         response = []
         for item in result:
-            response.append(datetime.timedelta(minutes=(item[0] / 60)))
+            response.append(datetime.timedelta(minutes=(item[0])))
 
         return response
--- application/statistics/views.py
+++ application/statistics/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from flask import render_template
 from flask_login import login_required, current_user
 
@@ -13,13 +15,19 @@ from application.categories.forms import CategoryForm
 @app.route("/statistics", methods=["GET"])
 @login_required
 def stats_view():
+    category_times = Statistics.time_spent_on_category(current_user.id)
+    overall_time = datetime.timedelta(0)
+    for i in category_times:
+        overall_time += i
+
     categories = list(Category.query.filter(Category.account_id == current_user.id))
     return render_template(
         "statistics/stats_view.html",
         categories=categories,
-        category_time=Statistics.time_spent_on_category(current_user.id),
+        category_time=category_times,
         categories_len=len(categories),
         events=list(Event.find_events_user_has_permissions_to(current_user.id)),
+        overall_time=overall_time,
         user=current_user,
         form=CategoryForm
     )
