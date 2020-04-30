62f3b70ab2b7f7396da5c11354d239b080b7c50b
Luukuton <mleinikka@gmail.com>
2020-04-30 05:29:11 +0300

    Fixed an operator match bug with PostgreSQL and added validator to Clip category selector

--- application/__init__.py
+++ application/__init__.py
@@ -53,7 +53,8 @@ def load_user(user_id):
     return User.query.get(user_id)
 
 
-# If there are any changes in the CSS file, this reloads it.
+# If there are any changes in the CSS file,
+# these two methods will initiate reloading of the CSS in the browser cache.
 @app.context_processor
 def override_url_for():
     return dict(url_for=dated_url_for)
